8fa49af84ecc49f26efc170f6d9f323927d68627
Janu shankar <janakiharishankar24@gmail.com>
2022-01-22 18:42:14 +0530

    sub program

---
+++ FetchJsonForRice.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace InventoryManagement
+{
+    internal class FetchJsonForRice
+    {
+        public Rice? ReadTheJsonFile(string Path)
+        {
+            using(StreamReader file=new StreamReader(Path))
+            { 
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return null; 
+                }
+               }
+        }
+    }
+}
