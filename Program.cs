09852ce95fd4f3f00a9c59cf49bd2660e91ce1b0
Janu shankar <janakiharishankar24@gmail.com>
2022-01-22 18:40:16 +0530

    Initial main program

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace InventoryManagement;
+
+public class Program
+{
+    static void Main(string[] args)
+
+    {
+        
+        FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+        Rice ricedata = fetchJsonForRice.ReadTheJsonFile("C:/Users/91936/new 1.json");
+        Console.WriteLine(ricedata);
+        for (int i = 0; i < ricedata.typesofRice.Count; i++)
+
+        {
+            Console.WriteLine(ricedata.typesofRice[i].name);
+            Console.WriteLine(ricedata.typesofRice[i].weight);
+            Console.WriteLine(ricedata.typesofRice[i].price);
+
+         Console.WriteLine("Total Price of {0} = {1}", ricedata.typesofRice[i].name, ricedata.typesofRice[i].weight * ricedata.typesofRice[i].price);
+
+        }
+        for (int i = 0; i < ricedata.typesofWheat.Count; i++)
+
+            {
+                Console.WriteLine(ricedata.typesofWheat[i].name);
+                Console.WriteLine(ricedata.typesofWheat[i].weight);
+                Console.WriteLine(ricedata.typesofWheat[i].price);
+
+            Console.WriteLine("Total price of {0} = {1}", ricedata.typesofWheat[i].name, ricedata.typesofWheat[i].weight * ricedata.typesofWheat[i].price);
+
+
+        }
+        for (int i = 0; i < ricedata.typesofPulses.Count; i++)
+
+            {
+                Console.WriteLine(ricedata.typesofPulses[i].name);
+                Console.WriteLine(ricedata.typesofPulses[i].weight);
+                Console.WriteLine(ricedata.typesofPulses[i].price);
+
+            Console.WriteLine("Total price of {0} = {1}", ricedata.typesofPulses[i].name, ricedata.typesofPulses[i].weight * ricedata.typesofPulses[i].price);
+
+
+        }
+
+    }
+    }
+
