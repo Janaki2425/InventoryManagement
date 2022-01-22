43b0e7261a0f21b40a2344376bdc1e36227a6781
Janu shankar <janakiharishankar24@gmail.com>
2022-01-22 18:42:55 +0530

    sub program

---
+++ Rice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    internal class Rice
+    {
+        public List<TypesOfRice> typesofRice;
+        public List<TypesOfWheat> typesofWheat;
+        public List<TypesOfPulses> typesofPulses;
+    }
+
+    public class TypesOfRice
+    {
+        public String name;
+        public int weight;
+        public String price;
+    }
+        public class TypesOfWheat
+        {
+            public String name;
+            public int weight;
+            public String price;
+        }
+        public class TypesOfPulses
+        {
+            public String name;
+            public int weight;
+            public String price;
+        }
+    }
+
